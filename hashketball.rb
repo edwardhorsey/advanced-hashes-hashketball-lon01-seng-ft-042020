6c6ca6e3b298d7b06e752c9d4f4d5345b14945f5
Edward Horsey <edwardhorsey@me.com>
2020-03-05 15:02:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -228,7 +228,7 @@ data.each { |key, value|
   index=0
   while index<value[:players].count do
     if value[:players][index][:points] == largest_shoe
-      return value[:players][index][:rebounds]
+      return value[:players][index][:player_name]
     end
     index+=1
   end
